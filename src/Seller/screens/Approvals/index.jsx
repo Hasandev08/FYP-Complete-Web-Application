d82f835df1c0b3a26062bcb4301381345236398c
Hasan <hasanahmed987123@gmail.com>
2023-04-17 02:05:55 +0500

    Approval Screen (In Progress)

--- src/Seller/screens/Approvals/index.jsx
+++ src/Seller/screens/Approvals/index.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 
+import SearchBar from '../../components/common/SearchBar'
 import Sidebar from '../../components/common/SideBar'
+
+import './style.css'
 
 const Approvals = () => {
   return (
     <div className='screen d-flex'>
       <div className='common-sidebar'>
         <Sidebar />
+      </div>
+      <div className='approvals d-flex flex-column w-100'>
+        <div className='approvals-search my-4'>
+          <SearchBar />
+        </div>
       </div>
     </div>
   )
